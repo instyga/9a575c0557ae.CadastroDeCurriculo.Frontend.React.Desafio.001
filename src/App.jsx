9ed91f910ff2d14e68712bada9276012cf8791cc
JuliaYuki <julia.yuki.monte.ribeiro@gmail.com>
2024-11-14 19:57:55 -0300

    gerencia o estado de cada formulário e o envio final dos dados, controlar a navegação entre as etapas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
 import { AppShell } from "@mantine/core";
 import { AppFooter } from "./components/app-footer";
 import { AppHeader } from "./components/app-header";
 import { Home } from "./pages/home";
 
 function App() {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [isStepValid, setIsStepValid] = useState(false);
+
+  const [personalData, setPersonalData] = useState({});
+  const [professionalExperiences, setProfessionalExperiences] = useState({});
+  const [scholarshipDetails, setScholarshipDetails] = useState({});
+
+  const nextStep = () => {
+    if (isStepValid && currentStep < 3) setCurrentStep((prevStep) => prevStep + 1);
+  };
+
+  const prevStep = () => {
+    if (currentStep > 1) setCurrentStep((prevStep) => prevStep - 1);
+  };
+
+  const handleValidationChange = (isValid) => {
+    setIsStepValid(isValid);
+  };
+
+  const handleFormDataChange = (step, data) => {
+    if (step === 1) setPersonalData(data);
+    if (step === 2) setProfessionalExperiences(data);
+    if (step === 3) setScholarshipDetails(data);
+  };
+
+  const handleSubmit = () => {
+    if (isStepValid) {
+      const allData = {
+        personalData,
+        professionalExperiences,
+        scholarshipDetails,
+      };
+      console.log("Dados enviados:", allData);
+      openConfirmSaveModal(() => alert("Dados enviados com sucesso!"))
+    }
+  };
+
   return (
     <AppShell
       header={{ height: 60 }}
@@ -15,10 +53,20 @@ function App() {
         <AppHeader />
       </AppShell.Header>
       <AppShell.Main>
-        <Home />
+        <Home
+          currentStep={currentStep}
+          onValidationChange={handleValidationChange}
+          onFormDataChange={handleFormDataChange}
+        />
       </AppShell.Main>
       <AppShell.Footer>
-        <AppFooter />
+        <AppFooter
+          currentStep={currentStep}
+          nextStep={nextStep}
+          prevStep={prevStep}
+          isStepValid={isStepValid}
+          onSubmit={handleSubmit}
+        />
       </AppShell.Footer>
     </AppShell>
   );
